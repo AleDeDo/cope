2adeeb6479fba6dff46cd1e30770216dd92f3998
AleDeDo <alessandrodedonatis7181@gmail.com>
2025-09-13 17:19:27 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,3 +5,11 @@ document.getElementById('menu-main').style.right = "0%"
 function menu_out() {
 document.getElementById('menu-main').style.right = "-100%"
 }
+
+function popup_in() {
+document.getElementById('popup').style.transform = "scale(1)"
+}
+
+function popup_out() {
+document.getElementById('popup').style.transform = "scale(0)"
+}
