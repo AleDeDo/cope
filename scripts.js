8347398afbb0dcb1b779b29119f9e0ccf5aa331e
AleDeDo <alessandrodedonatis7181@gmail.com>
2025-09-16 14:07:45 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -7,9 +7,12 @@ document.getElementById('menu-main').style.right = "-100%"
 }
 
 function popup_in() {
-document.getElementById('popup').style.transform = "scale(1)"
+document.getElementById('popup').style.transform = "scale(1)";
+document.getElementById('video1').play() 
 }
 
 function popup_out() {
 document.getElementById('popup').style.transform = "scale(0)"
+document.getElementById('video1').pause();
+document.getElementById('video1').currentTime = 0
 }
