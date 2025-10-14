cba0074ba0a4f97bcecf3bcc5844ce68a8944088
AleDeDo <alessandrodedonatis7181@gmail.com>
2025-10-14 13:01:50 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -26,6 +26,136 @@ document.getElementById('popup-text-2').style.transform = "scale(0)";
 document.getElementsByTagName('body')[0].style.overflow = "auto";
 }
 
+function popup_text_in_3() {
+document.getElementById('popup-text-3').style.transform = "scale(1)";
+document.getElementsByTagName('body')[0].style.overflow = "hidden";
+}
+
+function popup_text_out_3() {
+document.getElementById('popup-text-3').style.transform = "scale(0)";
+document.getElementsByTagName('body')[0].style.overflow = "auto";
+}
+
+function popup_text_in_4() {
+document.getElementById('popup-text-4').style.transform = "scale(1)";
+document.getElementsByTagName('body')[0].style.overflow = "hidden";
+}
+
+function popup_text_out_4() {
+document.getElementById('popup-text-4').style.transform = "scale(0)";
+document.getElementsByTagName('body')[0].style.overflow = "auto";
+}
+
+function popup_text_in_5() {
+document.getElementById('popup-text-5').style.transform = "scale(1)";
+document.getElementsByTagName('body')[0].style.overflow = "hidden";
+}
+
+function popup_text_out_5() {
+document.getElementById('popup-text-5').style.transform = "scale(0)";
+document.getElementsByTagName('body')[0].style.overflow = "auto";
+}
+
+function popup_text_in_6() {
+document.getElementById('popup-text-6').style.transform = "scale(1)";
+document.getElementsByTagName('body')[0].style.overflow = "hidden";
+}
+
+function popup_text_out_6() {
+document.getElementById('popup-text-6').style.transform = "scale(0)";
+document.getElementsByTagName('body')[0].style.overflow = "auto";
+}
+
+function popup_text_in_7() {
+document.getElementById('popup-text-7').style.transform = "scale(1)";
+document.getElementsByTagName('body')[0].style.overflow = "hidden";
+}
+
+function popup_text_out_7() {
+document.getElementById('popup-text-7').style.transform = "scale(0)";
+document.getElementsByTagName('body')[0].style.overflow = "auto";
+}
+
+function popup_text_in_8() {
+document.getElementById('popup-text-8').style.transform = "scale(1)";
+document.getElementsByTagName('body')[0].style.overflow = "hidden";
+}
+
+function popup_text_out_8() {
+document.getElementById('popup-text-8').style.transform = "scale(0)";
+document.getElementsByTagName('body')[0].style.overflow = "auto";
+}
+
+function popup_text_in_9() {
+document.getElementById('popup-text-9').style.transform = "scale(1)";
+document.getElementsByTagName('body')[0].style.overflow = "hidden";
+}
+
+function popup_text_out_9() {
+document.getElementById('popup-text-9').style.transform = "scale(0)";
+document.getElementsByTagName('body')[0].style.overflow = "auto";
+}
+
+function popup_text_in_10() {
+document.getElementById('popup-text-10').style.transform = "scale(1)";
+document.getElementsByTagName('body')[0].style.overflow = "hidden";
+}
+
+function popup_text_out_10() {
+document.getElementById('popup-text-10').style.transform = "scale(0)";
+document.getElementsByTagName('body')[0].style.overflow = "auto";
+}
+
+function popup_text_in_11() {
+document.getElementById('popup-text-11').style.transform = "scale(1)";
+document.getElementsByTagName('body')[0].style.overflow = "hidden";
+}
+
+function popup_text_out_11() {
+document.getElementById('popup-text-11').style.transform = "scale(0)";
+document.getElementsByTagName('body')[0].style.overflow = "auto";
+}
+
+function popup_text_in_12() {
+document.getElementById('popup-text-12').style.transform = "scale(1)";
+document.getElementsByTagName('body')[0].style.overflow = "hidden";
+}
+
+function popup_text_out_12() {
+document.getElementById('popup-text-12').style.transform = "scale(0)";
+document.getElementsByTagName('body')[0].style.overflow = "auto";
+}
+
+function popup_text_in_13() {
+document.getElementById('popup-text-13').style.transform = "scale(1)";
+document.getElementsByTagName('body')[0].style.overflow = "hidden";
+}
+
+function popup_text_out_13() {
+document.getElementById('popup-text-13').style.transform = "scale(0)";
+document.getElementsByTagName('body')[0].style.overflow = "auto";
+}
+
+function popup_text_in_14() {
+document.getElementById('popup-text-14').style.transform = "scale(1)";
+document.getElementsByTagName('body')[0].style.overflow = "hidden";
+}
+
+function popup_text_out_14() {
+document.getElementById('popup-text-14').style.transform = "scale(0)";
+document.getElementsByTagName('body')[0].style.overflow = "auto";
+}
+
+function popup_text_in_15() {
+document.getElementById('popup-text-15').style.transform = "scale(1)";
+document.getElementsByTagName('body')[0].style.overflow = "hidden";
+}
+
+function popup_text_out_15() {
+document.getElementById('popup-text-15').style.transform = "scale(0)";
+document.getElementsByTagName('body')[0].style.overflow = "auto";
+}
+
 function popup_video_in_1() {
 document.getElementById('popup-video-1').style.transform = "scale(1)";
 document.getElementsByTagName('body')[0].style.overflow = "hidden";
@@ -37,4 +167,50 @@ document.getElementById('popup-video-1').style.transform = "scale(0)";
 document.getElementsByTagName('body')[0].style.overflow = "auto";
 document.getElementById('video-1').pause();
 document.getElementById('video-1').currentTime = 0;
+}
+
+function popup_audio_in_1() {
+document.getElementById('popup-audio-1').style.transform = "scale(1)";
+document.getElementsByTagName('body')[0].style.overflow = "hidden";
+document.getElementById('audio-1').play(); 
+}
+
+function popup_audio_out_1() {
+document.getElementById('popup-audio-1').style.transform = "scale(0)";
+document.getElementsByTagName('body')[0].style.overflow = "auto";
+document.getElementById('audio-1').pause();
+document.getElementById('audio-1').currentTime = 0;
+}
+
+function popup_audio_in_2() {
+document.getElementById('popup-audio-2').style.transform = "scale(1)";
+document.getElementsByTagName('body')[0].style.overflow = "hidden";
+document.getElementById('audio-2').play(); 
+}
+
+function popup_audio_out_2() {
+document.getElementById('popup-audio-2').style.transform = "scale(0)";
+document.getElementsByTagName('body')[0].style.overflow = "auto";
+document.getElementById('audio-2').pause();
+document.getElementById('audio-2').currentTime = 0;
+}
+
+function popup_audio_in_3() {
+document.getElementById('popup-audio-3').style.transform = "scale(1)";
+document.getElementsByTagName('body')[0].style.overflow = "hidden";
+document.getElementById('audio-3').play(); 
+}
+
+function popup_audio_out_3() {
+document.getElementById('popup-audio-3').style.transform = "scale(0)";
+document.getElementsByTagName('body')[0].style.overflow = "auto";
+document.getElementById('audio-3').pause();
+document.getElementById('audio-3').currentTime = 0;
+}
+
+function gallery(imgs) {
+  var gallery_img = document.getElementById("gallery_img");
+  var gallery_text = document.getElementById("gallery_text");
+  gallery_img.src = imgs.src;
+  gallery_text.innerHTML = imgs.alt;
 }
