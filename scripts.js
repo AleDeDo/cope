12d8da97a117098b725f537c1a5c54d6c3b8e8cc
AleDeDo <alessandrodedonatis7181@gmail.com>
2026-01-16 13:12:05 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -215,16 +215,24 @@ function gallery(imgs) {
   gallery_text.innerHTML = imgs.alt;
 }
 
-var map = L.map('map').setView([39.4714069, -0.3829845], 13);
-      	 
-      	 //CartoDB layer names: light_all / dark_all / light_nonames / dark_nonames
-      	 var layer = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
-            attribution: ''
-         });
-      	 
-         layer.addTo(map);
+var banner = 1;
+var links = ['https://coocv.com/', 'https://gestoresvalencia.org/', 'https://vegamar.es/', 'https://innovalogic.es/'];
+var link = links[0];
+
+setInterval(function () {
+    
+    if (banner >= 1 && banner <= 5) {
+        link = links[banner - 1];
+    }
   
-      var marker = L.marker([39.4714069, -0.3829845]).addTo(map);
-      marker.bindPopup("<img src='https://alededo.github.io/cope/img/fallas-1.jpg' style='width:200px'/><h3>Falla Plaza del Pilar</h3><p>Falla Sección Especial</p><p style='text-align:center'><a href='google.com/maps/dir//39.4714069,-0.3829845' target='_blank'><i class='material-symbols-outlined circle'>arrow_top_right</i></a></p>");
+  var url = 'https://alededo.github.io/cope/img/banner-' + banner + '.jpg';
 
+    document.getElementById('banner').src = url;
+    document.getElementById('banner').href = link;
 
+    banner++;
+    if (banner > 4) {
+        banner = 1;
+    }
+
+}, 3000);
